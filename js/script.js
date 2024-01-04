ad65134e6c3cfa59d276002bd93f9111e7bcbbb1
TheGrinchOnMath <jonathanwakeman@hotmail.com>
2024-01-04 20:42:37 +0100

    added content, images and ugly css

--- js/script.js
+++ js/script.js
@@ -4,19 +4,34 @@ script for the motorpool tool
 
 //Global Variables
 let factionsTree = null,
-	motorpoolMap = null;
+	motorpoolMap = new Map(),
+	motorpoolTree = null;
 
 async function parse(factionsPath, motorpoolPath) {
 	factionsTree = await parseJson(factionsPath);
-	motorpoolMap = await parseMotorpool(parseJson(motorpoolPath));
+	motorpoolTree = await parseJson(motorpoolPath);
 
+	//----------test----------//
+	//console.log(motorpoolTree[0]);
+	//------------------------//
 	let sides = new Array();
 	for (let side in factionsTree) {
 		sides.push(side);
 	}
+
+	for (let i = 0; i < motorpoolTree.length; i++) {
+		const motorpool = motorpoolTree[i];
+		motorpoolMap.set(motorpool.id, [
+			motorpool.name,
+			motorpool.type,
+			motorpool.crew,
+			motorpool.passengers,
+			motorpool.cargo,
+			motorpool.turrets,
+		]);
+	}
+
 	initSides(sides);
-	initFactions("blufor");
-	variants(factionsTree.blufor[0]);
 }
 
 async function parseJson(path) {
@@ -28,54 +43,28 @@ async function parseJson(path) {
 	return response;
 }
 
-// this function parses the json obj to get a dict.
-function parseFactions(json) {
-	const sides = { blufor: {}, opfor: {}, indep: {} }; // JSON contains: side:faction:variants/name:motorpool/name/era:vehicles:vehicle
-	// sides should look like this: sides.blufor.factionName.variant
-	// sides.blufor["woodland modern"] = []
-	// sides.blufor.["woodland modern"].push([])
-	// fill blufor with data
-	for (let ctr = 0; ctr < json.blufor.length; ctr++) {
-		let name = json.blufor[ctr].name;
-		let id = name;
-		id.replace(" ", "_");
-		sides.blufor[id] = [];
-		for (let count = 0; count < json.blufor[ctr].variants.length; count++)
-			sides.blufor[id].push([
-				json.blufor[ctr].variants[count].name,
-				json.blufor[ctr].variants[count].era,
-			]);
-	}
-	console.log(sides.blufor);
-	// json.side[index].name gets the faction name
-
-	/*
-	add here: some kind of loop contraption that reads the json and fills the required data into sides. the idea is to make the data easily accessible later.
-	*/
-
-	return sides;
-}
-
 //this function parses the motorpool json obj into a map obj, with the ID being the name,
 //and the other informations inside a list being the value
+/*
 function parseMotorpool(json) {
-	out = new Map();
-	for (let i = 0; i < json.length; i++) {
-		Map[json[i].id] = [
-			json[i].name,
-			json[i].type,
-			json[i].crew,
-			json[i].passengers,
-			json[i].cargo,
-			json[i].turrets,
-		];
+	if (json != null) {
+		out = new Map();
+
+		for (let i = 0; i < json.length; i++) {
+			out.set(json[i].id, [
+			]);
+		}
+		return out;
+	} else {
+		console.log("parseMotorpool: input was null for some reason");
 	}
-	return out;
 }
+*/
 
 /* 
 this function is dedicated to creating a button DOM in the html
-the context manager uses context as input, and the function then takes input:data and can be called using context.input
+the context manager uses context as input, and the function then takes 
+input:data and can be called using context.input
 */
 function createButton(id, parent, position, func, params) {
 	// check if the button already exists
@@ -119,7 +108,7 @@ function removeChildren(id) {
 	}
 }
 
-async function image(id, parent, position, url) {
+function image(id, parent, position, url) {
 	// get the node of the parent, create image node
 	const parentDOM = document.getElementById(parent);
 	let imageDOM = document.createElement("img");
@@ -139,10 +128,11 @@ function randomImage(id, parent, position) {
 
 	// set attributes
 	imageDOM.setAttribute("id", id);
-	imageDOM.setAttribute("src", "https://cataas.com/cat?width=:256&height=:256");
+	imageDOM.setAttribute("src", "https://cataas.com/cat?width=256&height=256");
 
 	parentDOM.insertAdjacentElement(position, imageDOM);
 }
+
 /* 
 is called upon loading the page. creates side buttons
 */
@@ -159,14 +149,6 @@ function initSides(sideList) {
 		createButton(side, "sides", "beforeend", initFactions, side);
 	}
 }
-
-/*
-sides() is called when a side button is clicked
-
-function sides() {
-
-}
-*/
 
 // create factions based on which side was called
 function initFactions(side) {
@@ -211,19 +193,65 @@ this function has the task of generating the content of the variant.
 This includes the vehicle stats, an image and the input.
 */
 async function generateContent(variant) {
-	// get all vehicle groups
+	// get all vehicle groups, get motorpool
 
+	// iterate through motorpool groups, create a div for every vehicle, add a cat photo
 	for (let i = 0; i < variant.motorpool.length; i++) {
+		// create new variable for every group in the motorpool.
+		// Create p for group, fetch content div
 		const vehicleGroup = variant.motorpool[i];
-		groupDiv = document.createElement("div");
-		groupDiv.setAttribute("id", vehicleGroup.name);
+		const contentDiv = document.getElementById("content");
+
+		// create p element, add attributes, content and add to groupDiv
+		let groupTitle = document.createElement("p");
+		groupTitle.innerText = vehicleGroup.group;
+		groupTitle.id = vehicleGroup.group;
+		contentDiv.insertAdjacentElement("beforeend", groupTitle);
+
+		// start iterating through the vehicles
+		const vehicles = vehicleGroup.vehicles;
+
+		for (let j = 0; j < vehicles.length; j++) {
+			//console.log(vehicles[j].id, vehicles[j].cargo);
+			//console.log(motorpoolMap.get(vehicles[j].id));
+
+			const data = motorpoolMap.get(vehicles[j].id);
+			// avoid errors cropping up when data is undefined
+			if (data == undefined) {
+				console.log(data, vehicles[j].id, motorpoolMap.get(vehicles[j].id));
+				console.log(vehicles[13]);
+				console.log("data is undefined");
+			} else if (data != undefined) {
+				//construct string to display inside of vehicleData
+				const string =
+					"Name: " +
+					data[0] +
+					"\tType: " +
+					data[1] +
+					"<br/>Crew: " +
+					data[2] +
+					"\tPassengers: " +
+					data[3] +
+					"<br/>cargo:" +
+					vehicles[j].cargo;
+				if (data[4].length > 0) {
+					string.concat("<br/>", data[4]);
+				}
+				// create and fill DOM elements
+				let vehicleDiv = document.createElement("div"),
+					vehicleData = document.createElement("p");
+				vehicleDiv.setAttribute("id", "vehicleContainer");
+				contentDiv.insertAdjacentElement("afterbegin", vehicleDiv);
+				vehicleData.innerHTML = string;
+				
+				let image = document.createElement("img");
+				image.setAttribute("src", "https://cataas.com/cat?width=128&height=128")
+				image.setAttribute("alt", " placeholder text");
+				vehicleDiv.insertAdjacentElement("afterbegin", image);
+				vehicleDiv.insertAdjacentElement("beforeend", vehicleData);
+			}
+		}
 	}
-
-	console.log(variant);
-
-	const url = "https://cataas.com/cat?width=:256&height=:256";
-	const id = "image";
-	parent = "";
 }
 
 //----------VARIABLES----------//
@@ -234,3 +262,4 @@ const motorpoolPath = "../json/motorpool.json";
 //------------TESTING BELOW HERE FOR READABILITY--------------//
 
 parse(factionsPath, motorpoolPath);
+console.log(motorpoolMap.get("Car"));
